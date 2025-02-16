349fba484eb7a5359e6b749e92ec89c14f0664cb
javier-albino <javier.sandoval.temuco@gmail.com>
2025-02-15 23:22:20 -0300

    "fix code"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+
 import { JwtStrategy } from '../jwt.strategy';
 
 @Module({

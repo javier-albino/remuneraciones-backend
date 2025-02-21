88539bf3572436a0bf996f9708cc9286d901047a
javier-albino <javier.sandoval.temuco@gmail.com>
2025-02-21 16:08:40 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 🔹 Habilitar CORS para permitir conexiones desde React
+  // 🔹 Definir los orígenes permitidos (desarrollo y producción)
+  const allowedOrigins = [
+    'http://localhost:5173', // Desarrollo
+    'https://publicar-aws.d3jgg0pv126bjm.amplifyapp.com', // Producción en AWS Amplify
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:5173', // Cambia esto a la URL de tu frontend
+    origin: allowedOrigins, // Permite varias URLs
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Si usas cookies o autenticación con sesión
+    credentials: true, // Si usas cookies o JWT con credenciales
   });
 
   await app.listen(3000, '0.0.0.0');
